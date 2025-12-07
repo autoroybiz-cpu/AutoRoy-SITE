cc69703157c3c72bb1f70d91fc65f78be6b4e8ef
Roy Aharonovich <autoroybiz@gmail.com>
2025-12-07 03:26:17 +0200

    Update theme.js

--- js/theme.js
+++ js/theme.js
@@ -1,1 +1,8 @@
-
+document.querySelectorAll(".theme-toggle button").forEach(btn => {
+btn.addEventListener("click", () => {
+document.body.classList.remove("dark","cyber");
+if (btn.dataset.mode !== "light") {
+document.body.classList.add(btn.dataset.mode);
+}
+});
+});
